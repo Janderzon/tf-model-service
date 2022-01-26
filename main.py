5d2017005c67a6dd28e85730d1d0c90242b08040
James <james.anderson2@btinternet.com>
2022-01-26 22:21:07 +0000

    Created listen function

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import server
+import socket
 
-HOST = '127.0.0.1'
 PORT = 65432
 
-server.start(HOST, PORT)
+
+def listen(PORT):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('', PORT))
+        s.listen()
+        return s.accept()
