38ac8aa886988ea54abe26d21ae34b8fac4d3a95
James Anderson <james.anderson2@btinternet.com>
2022-01-31 22:28:24 +0000

    Moved request processing logic to separate class

--- main.py
+++ main.py
@@ -2,31 +2,9 @@ import json
 import socket
 import tensorflow as tf
 from tensorflow import keras
+from requests import RequestProcessor
 
 PORT = 65432
-
-
-class ReturnObjectManager:
-    def __init__(self, type):
-        self.dict = dict()
-        self.dict['type'] = type
-
-    def set_succeeded(self, success):
-        self.dict['succeeded'] = success
-
-    def set_return_data(self, data):
-        self.dict['data'] = data
-
-    def set_error_message(self, message):
-        self.dict['error'] = message
-
-    def get_return_obj(self):
-        return self.dict
-
-    def contains(self, key):
-        if key in self.dict:
-            return True
-        return False
 
 
 class Model:
@@ -71,70 +49,16 @@ def read_data(connection):
     return data
 
 
-def process_request(json_obj, model, data):
-    request_type = json_obj['type']
-    if request_type == 'model':
-        return read_model(json_obj, model)
-
-    elif request_type == 'input_data':
-        return read_input_data(json_obj, data)
-
-    elif request_type == 'predict':
-        return make_prediction(model, data)
-
-
-def read_model(json_obj, model):
-    obj_manager = ReturnObjectManager('read_model')
-
-    if obj_manager.contains('model'):
-        model.set_model(json_obj['model'])
-        obj_manager.set_succeeded(True)
-    else:
-        obj_manager.set_error_message('No model provided')
-        obj_manager.set_succeeded(False)
-
-    return obj_manager.get_return_obj()
-
-
-def read_input_data(json_obj, data):
-    obj_manager = ReturnObjectManager('loaded_input_data')
-
-    if obj_manager.contains('data'):
-        data.set_input_data(json_obj['data'])
-        obj_manager.set_succeeded(True)
-    else:
-        obj_manager.set_error_message('No input data provided')
-        obj_manager.set_succeeded(False)
-
-    return obj_manager.get_return_obj()
-
-
-def make_prediction(model, data):
-    obj_manager = ReturnObjectManager('made_prediction')
-    obj_manager.set_succeeded(False)
-
-    model = model.get_model()
-    data = data.get_input_data()
-
-    if model is None:
-        obj_manager.set_error_message('No model loaded')
-        return obj_manager.get_return_obj()
-
-    if data is None:
-        obj_manager.set_error_message('No input data')
-        return obj_manager.get_return_obj()
-
-    return obj_manager.get_return_obj()
-
-
 model = Model()
 input_data_manager = InputDataManager()
+request_processor = RequestProcessor()
 connection, address = listen(PORT)
 with connection:
     data = read_data(connection)
     json_data = json.loads(data)
     return_objs = []
     for json_obj in json_data:
-        return_obj = process_request(json_obj, model, input_data_manager)
+        return_obj = request_processor.process_request(
+            json_obj, model, input_data_manager)
         return_objs.append(return_obj)
     print(json.dumps(return_objs))
---
+++ requests.py
@@ -0,0 +1,75 @@
+class ReturnObjectManager:
+    def __init__(self, type):
+        self.dict = dict()
+        self.dict['type'] = type
+
+    def set_succeeded(self, success):
+        self.dict['succeeded'] = success
+
+    def set_return_data(self, data):
+        self.dict['data'] = data
+
+    def set_error_message(self, message):
+        self.dict['error'] = message
+
+    def get_return_obj(self):
+        return self.dict
+
+    def contains(self, key):
+        if key in self.dict:
+            return True
+        return False
+
+
+class RequestProcessor:
+    def _read_model(self, json_obj, model):
+        obj_manager = ReturnObjectManager('read_model')
+
+        if obj_manager.contains('model'):
+            model.set_model(json_obj['model'])
+            obj_manager.set_succeeded(True)
+        else:
+            obj_manager.set_error_message('No model provided')
+            obj_manager.set_succeeded(False)
+
+        return obj_manager.get_return_obj()
+
+    def _read_input_data(self, json_obj, data):
+        obj_manager = ReturnObjectManager('loaded_input_data')
+
+        if obj_manager.contains('data'):
+            data.set_input_data(json_obj['data'])
+            obj_manager.set_succeeded(True)
+        else:
+            obj_manager.set_error_message('No input data provided')
+            obj_manager.set_succeeded(False)
+
+        return obj_manager.get_return_obj()
+
+    def _make_prediction(self, model, data):
+        obj_manager = ReturnObjectManager('made_prediction')
+        obj_manager.set_succeeded(False)
+
+        model = model.get_model()
+        data = data.get_input_data()
+
+        if model is None:
+            obj_manager.set_error_message('No model loaded')
+            return obj_manager.get_return_obj()
+
+        if data is None:
+            obj_manager.set_error_message('No input data')
+            return obj_manager.get_return_obj()
+
+        return obj_manager.get_return_obj()
+
+    def process_request(self, json_obj, model, data):
+        request_type = json_obj['type']
+        if request_type == 'model':
+            return self._read_model(json_obj, model)
+
+        elif request_type == 'input_data':
+            return self._read_input_data(json_obj, data)
+
+        elif request_type == 'predict':
+            return self._make_prediction(model, data)
