a7233241ab2d66ed5b5f6acd9f63b976f8ac0410
James Anderson <james.anderson2@btinternet.com>
2022-02-19 18:19:44 +0000

    Updated error response

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def predict():
         prediciton = _model.make_prediction()
     except ValueError as error:
         error = flask.jsonify(error=str(error))
-        flask.abort(error, 400)
+        return error, 400
     return {
         'prediction': prediciton
     }
